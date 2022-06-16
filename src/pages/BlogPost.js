7d584e1f71de53e858d291d2bf56204fe0a11d00
Ryan Peacock <ryanxtry@gmail.com>
2022-06-16 14:32:19 -0700

    updated single blog post style

--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -1,14 +1,14 @@
 import React, { useContext, useEffect } from "react";
+import moment from "moment";
 import AppContext from "../context/app/appContext";
 import * as FaIcons from "react-icons/fa";
-import Layout from "../components/layout/Layout";
+import Header from "../components/layout/Header";
 
 const SinglePost = ({ data }) => {
   const { id, title, timestamp, text } = data;
   return (
-    <div>
-      <h1>{title}</h1>
-      <span>{timestamp}</span>
+    <div className="post-page">
+      <span>{moment(timestamp).format("DD MMM YYYY")}</span>
       <div className="content" dangerouslySetInnerHTML={{ __html: text }} />
     </div>
   );
@@ -33,16 +33,27 @@ const BlogPost = ({
   }, []);
 
   return (
-    <Layout>
+    <div className="single-post-page">
+      <Header />
       {Object.keys(singleBlogPost).length === 0 ? (
         <h2>Loading...</h2>
       ) : (
-        <div className="single-post-page">
-          <FaIcons.FaArrowLeft onClick={() => history.push("/")} />
-          <SinglePost data={singleBlogPost} />
-        </div>
+        <>
+          <div className="title-block">
+            <div className="overlay">
+              <h1> {singleBlogPost.title}</h1>
+            </div>
+          </div>
+          <div className="container">
+            <FaIcons.FaArrowLeft
+              onClick={() => history.push("/")}
+              className="back-arrow"
+            />
+            <SinglePost data={singleBlogPost} />
+          </div>
+        </>
       )}
-    </Layout>
+    </div>
   );
 };
 
